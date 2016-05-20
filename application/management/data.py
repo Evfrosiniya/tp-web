bd88326ba54f0762ae7973c0c5befaea42f56584
Evfrosiniya <evfrosiniya.z@gmail.com>
2016-05-20 15:29:34 +0300

    20 05

---
+++ application/management/data.py
@@ -0,0 +1,4 @@
+from django.core.management.base import BaseCommand, CommandError
+from application.models import *
+from django.contrib.auth.models import User
+from django.utils import timezone
--- ask_zerminova/urls.py
+++ ask_zerminova/urls.py
@@ -6,19 +6,19 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^hot/(?P<page>\d+)/$', views.hot_index, name='hot_index'),
-    url(r'^hot/$', views.hot_index, name='hot_index'),
+    #url(r'^hot/$', views.hot_index, name='hot_index'),
 
     url(r'^tag/(?P<tag>\d+)//(?P<page>\d+)/$', views.tag_index, name='tag_index'),
-    url(r'^tag/$', views.tag_index, name='tag_index'),
+    #url(r'^tag/$', views.tag_index, name='tag_index'),
 
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', views.login, name='login'),
     url(r'^ask/$', views.ask, name='ask'),
 
     url(r'^question/(?P<question_id>\d+)/(?P<page>\d+)/$', views.question, name='question'),
-    url(r'^question/(?P<page>\d+)/$', views.question, name='question'),
-    url(r'^question/$', views.question, name='question'),
+    #url(r'^question/(?P<page>\d+)/$', views.question, name='question'),
+    #url(r'^question/$', views.question, name='question'),
 
     url(r'^(?P<page>\d+)/$', views.index, name='index'),
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
 ]
